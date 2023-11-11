460f2ae67d70fe1dfc0ed7f9f3e745ed25ffa7a5
Walter <tebbettswt@gmail.com>
2023-11-11 16:33:43 -0500

    added .env to hide ip address

--- backend/testAPI.py
+++ backend/testAPI.py
@@ -1,5 +1,10 @@
+import os
 from flask import Flask,jsonify,request
 from flask_cors import CORS
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
+IP_ADDRESS = os.environ.get("RPI_IP_ADDRESS")
 
 app = Flask(__name__)
 CORS(app)
@@ -13,4 +18,4 @@ def hello():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host=f'{IP_ADDRESS}')
