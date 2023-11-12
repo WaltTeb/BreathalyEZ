e4741d2528caeb189a2d0b1dd7201983e3a22f1c
Walter <tebbettswt@gmail.com>
2023-11-12 01:09:51 -0500

    added statements to frontend

--- frontend/breathez/src/App.tsx
+++ frontend/breathez/src/App.tsx
@@ -1,62 +1,82 @@
 import './App.css';
 import { useState } from 'react';
-import { useEffect } from 'react';
 import Button from '@mui/material/Button';
-import CircularProgress from '@mui/material/CircularProgress';
 
-// const RSP_IP = {process.env.REACT_APP_RPI_IP_ADDRESS}
 
-function handleCollect(setCollecting:any, setProgress:any, prevProgress:number) {
-  setCollecting(true);
-  const timer = setInterval(() => {
-    if(prevProgress <= 95){
-      prevProgress += 5;
-      setProgress(prevProgress)
-    }else{
-      setProgress(0);
-      clearInterval(timer);
-      setCollecting(false);
-    }
-  }, 200)
-
-  
-
-  return;
+const ResponseMessage = (props: { BAC: string }) => {
+  let resMessage = "";
+  let testBAC: number = Number(props.BAC);
+  if (testBAC == 0.00) {
+    resMessage = "Safe BAC Level!"
+  }
+  else if ((testBAC > 0.00) && (testBAC < 0.08)) {
+    resMessage = "Below Legal Limit, Please Use Discretion."
+  }
+  else if (testBAC < 0.00) {
+    resMessage = "An Error Occurred, Please Try Again."
+  }
+  else {
+    resMessage = "Above Legal Limit, DO NOT DRIVE!"
+  }
+  return (
+    <p>{resMessage}</p>
+  )
 }
 
 function App() {
   const [collecting, setCollecting] = useState<boolean>(false);
-  const [progress, setProgress] = useState<number>(0);
+  const [BAC, setBAC] = useState("0.000");
 
   const rpi_address = process.env.REACT_APP_RPI_IP_ADDRESS;
 
-  // useEffect(() => {
-  //   fetch(`http://${rpi_address}:5000/hello`)
-  //   .then(res => res.json())
-  //   .then(data => console.log(data.welcome))
-  //   .catch(err => console.log(err))
-  // })
-  
+  async function handleCollectSample() {
+    fetch(`http://${rpi_address}:5000/hello`)
+      .then(res => res.json())
+      .then((data) => {
+        let BACVal: number = data.BAC;
 
-  return(
+
+        setBAC(String(BACVal.toFixed(3)));
+        setCollecting(false);
+      })
+      .catch(err => console.log(err))
+  }
+
+
+  return (
     <>
+
       <div id="basicPage">
         <div id="titleCard">
           <h1>BreathalyEZ</h1>
         </div>
         <div id="lowerPage">
-          {collecting && <CircularProgress 
-          variant='determinate'
-          value={progress}
-          id="collectionProgress"
-          />}
+          <div id="textArea">
+
+            {collecting ?
+              <p>Collecting Sample...</p> :
+              <p>{`BAC = ${BAC}`}</p>}
+
+          </div>
+
           <Button
-          variant="contained"
-            onClick={() => handleCollect(setCollecting, setProgress, progress)}
+            disabled={collecting}
+            id="collectSampleButton"
+            variant="contained"
+            onClick={() => {
+              setCollecting(true);
+              handleCollectSample()
+            }
+            }
+
           >
             Collect Sample</Button>
         </div>
+        <div>
+          <ResponseMessage BAC={BAC} />
+        </div>
       </div>
+
     </>
   )
 }
