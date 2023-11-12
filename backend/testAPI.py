e26b36718c9371c3fcf741cae8dde715d175792a
Walter <tebbettswt@gmail.com>
2023-11-11 23:14:03 -0500

    got serverside data collection and processing working

--- backend/testAPI.py
+++ backend/testAPI.py
@@ -10,16 +10,32 @@ def hello():
     if(request.method == 'GET'):
         collectSample = []
         ser = serial.Serial('/dev/ttyACM0')
-        for i in range(100):
-            x = ser.readline()
-            x = x.decode('UTF-8')
-            y = x.split('\r')
-            if len(y) == 4:
-                collectSample.append(float(x))
-        average = sum(collectSample)/100
+        average = "blank"
+        try:
+            for i in range(100):
+                x = ser.readline()
+                try:
+                    x = x.decode('UTF-8')
+                    y = x.split('\r')[0]
+                    collectSample.append(float(y))
+                except:
+                    y = 0.00
+            average = sum(collectSample)/len(collectSample)
+        except Exception as e:
+            average = f'error occured, exception = {e}'
 
+        v = average
+        ppm = 150.4351049*pow(v, 5) - 2244.75988*pow(v, 4) + 13308.5139*pow(v, 3) -39136.08594*pow(v, 2) + 57082.6258*v - 32982.05333
+
+        if ppm <= 70:
+            bac = 0
+        elif ppm >= 500:
+            bac = -1
+        else:
+            bac = ppm / 2600
+        
         data = {
-            "ppm": average
+            "BAC": bac
         }
 
     return jsonify(data)
